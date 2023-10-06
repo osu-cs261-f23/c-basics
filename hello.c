ffaf5c95e980bf2bbeb04bfac43d78ff8ef70852
Rob Hess <hessro@oregonstate.edu>
2023-10-06 10:53:03 -0700

    Add code from 10/6 lecture.

--- hello.c
+++ hello.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+struct student {
+    char* name;
+    int id;
+    float gpa;
+};
+
+void print_student_name(struct student s) {
+
+}
+
+int add_3_ints(int a, int b, int c);
+
+void foo(int x) {
+    printf("foo was passed this value: %d\n", x);
+}
+
 int main() {
     int n = 16;
     double d = 3.1415;
@@ -27,6 +43,26 @@ int main() {
         printf("n is between 0 and 10\n");
     }
 
+    for (int i = 0; i < 8; i++) {
+        printf("-- i: %d\n", i);
+    }
+
+    int sum = add_3_ints(1, 2, 3);
+    printf("sum: %d\n", sum);
+    printf("add_3_ints(10, 11, 12): %d\n", add_3_ints(10, 11, 12));
+
+    foo(32);
+
+    struct student s;
+    s.name = "Luke Skywalker";
+    s.id = 933111111;
+    s.gpa = 3.75;
+    printf("%s (%d) gpa: %f\n", s.name, s.id, s.gpa);
+
     // Single line comment
     return 0;
+}
+
+int add_3_ints(int a, int b, int c) {
+    return a + b + c;
 }
